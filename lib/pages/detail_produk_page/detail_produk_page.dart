f081d0d79c94e338c31f3ae4d1a5ae6ff3ad4ee0
codeuzix@gmail.com <satrialipatullah@gmail.com>
2023-11-11 19:23:46 +0800

    as

--- lib/pages/detail_produk_page/detail_produk_page.dart
+++ lib/pages/detail_produk_page/detail_produk_page.dart
@@ -24,14 +24,12 @@ class _DetailProdukPageState extends State<DetailProdukPage> {
   final addKeranjangCubit = BaseLogic<KeranjangRes>();
   final catatanCon = TextEditingController();
   void getData() async {
-    await getCubit.fetch(
-        Repositories.getSingleProduk, ProdukRes(kode: widget.kode));
+    await getCubit.fetch(Repositories.getSingleProduk, ProdukRes(kode: widget.kode));
   }
 
   void getKeranjangData() async {
     if (widget.kodeKeranjang == null) return;
-    final res = await getKeranjangCubit.fetch(Repositories.getSingleKeranjang,
-        KeranjangRes(kode: widget.kodeKeranjang));
+    final res = await getKeranjangCubit.fetch(Repositories.getSingleKeranjang, KeranjangRes(kode: widget.kodeKeranjang));
     if (res is BaseLogicSuccess<BaseResponse<KeranjangRes>>) {
       catatanCon.text = res.data?.data?.catatan ?? "";
       total = res.data?.data?.total ?? 0;
@@ -41,12 +39,7 @@ class _DetailProdukPageState extends State<DetailProdukPage> {
 
   void updateKeranjangData() async {
     if (widget.kodeKeranjang == null) return;
-    final res = await getKeranjangCubit.fetch(
-        Repositories.updateKeranjang,
-        KeranjangRes(
-            kode: widget.kodeKeranjang,
-            catatan: catatanCon.text,
-            total: total));
+    final res = await getKeranjangCubit.fetch(Repositories.updateKeranjang, KeranjangRes(kode: widget.kodeKeranjang, catatan: catatanCon.text, total: total));
     if (res is BaseLogicSuccess<BaseResponse<KeranjangRes>>) {
       context.pop();
     }
@@ -63,19 +56,14 @@ class _DetailProdukPageState extends State<DetailProdukPage> {
       ),
     );
     if (res is BaseLogicSuccess<BaseResponse<KeranjangRes>>) {
-      context.read<BaseLogic<List<KeranjangRes>>>().fetch(
-          Repositories.getAllKeranjangByUser,
-          locator<SettingCubit>().state.kd!);
+      context.read<BaseLogic<List<KeranjangRes>>>().fetch(Repositories.getAllKeranjangByUser, locator<SettingCubit>().state.kd!);
       InfoCard(
         InfoType.success,
         message: "Berhasil di masukan ke keranjang",
       ).show(context);
     }
     if (res is BaseLogicError) {
-      InfoCard(InfoType.error,
-              message:
-                  res.failure.response.data.toString().replaceAll("\"", ""))
-          .show(context);
+      InfoCard(InfoType.error, message: res.failure.response.data.toString().replaceAll("\"", "")).show(context);
     }
   }
 
@@ -123,12 +111,7 @@ class _DetailProdukPageState extends State<DetailProdukPage> {
                     child: Container(
                       width: double.maxFinite,
                       height: Dimensions.popularFoodImgSize(context),
-                      decoration: BoxDecoration(
-                          image: DecorationImage(
-                              fit: BoxFit.cover,
-                              image: NetworkImage(
-                                  locator<Htreq>().base.baseUrl +
-                                      (data.data?.foto ?? "")))),
+                      decoration: BoxDecoration(image: DecorationImage(fit: BoxFit.cover, image: NetworkImage(locator<Htreq>().base.baseUrl + (data.data?.foto ?? "")))),
                     )),
                 //icon widget
                 Positioned(
@@ -138,16 +121,13 @@ class _DetailProdukPageState extends State<DetailProdukPage> {
                     child: Row(
                       mainAxisAlignment: MainAxisAlignment.spaceBetween,
                       children: [
-                        GestureDetector(
-                            onTap: () => context.pop(),
-                            child: const AppIcon(icon: Icons.arrow_back_ios)),
+                        GestureDetector(onTap: () => context.pop(), child: const AppIcon(icon: Icons.arrow_back_ios)),
                         if (widget.kodeKeranjang == null)
                           InkWell(
                             onTap: () => context.push(const KeranjangPage()),
                             child: Stack(
                               children: [
-                                const AppIcon(
-                                    icon: Icons.shopping_cart_outlined),
+                                const AppIcon(icon: Icons.shopping_cart_outlined),
                                 const Positioned(
                                   right: 0,
                                   top: 0,
@@ -162,16 +142,14 @@ class _DetailProdukPageState extends State<DetailProdukPage> {
                                   right: 5,
                                   top: 5,
                                   child: BaseBlocBuilder(
-                                    cubit: context
-                                        .read<BaseLogic<List<KeranjangRes>>>(),
+                                    cubit: context.read<BaseLogic<List<KeranjangRes>>>(),
                                     onLoading: (cubit) => const SizedBox(
                                       height: 12,
                                       width: 12,
                                       child: CircularProgressIndicator(),
                                     ),
                                     onSuccess: (cubit, data) => BigText(
-                                      text:
-                                          data.data?.length.toCurrency() ?? "0",
+                                      text: data.data?.length.toCurrency() ?? "0",
                                       size: 12,
                                       color: Colors.white,
                                     ),
@@ -187,34 +165,20 @@ class _DetailProdukPageState extends State<DetailProdukPage> {
                     left: 0,
                     right: 0,
                     bottom: 0,
-                    top: (Dimensions.popularFoodImgSize(context) - 20) -
-                        MediaQuery.of(context).viewInsets.bottom,
+                    top: (Dimensions.popularFoodImgSize(context) - 20) - MediaQuery.of(context).viewInsets.bottom,
                     child: Container(
-                      padding: EdgeInsets.only(
-                          left: Dimensions.width20(context),
-                          right: Dimensions.width20(context),
-                          top: Dimensions.height20(context)),
-                      decoration: BoxDecoration(
-                          borderRadius: BorderRadius.only(
-                              topRight:
-                                  Radius.circular(Dimensions.radius20(context)),
-                              topLeft: Radius.circular(
-                                  Dimensions.radius20(context))),
-                          color: Colors.white),
+                      padding: EdgeInsets.only(left: Dimensions.width20(context), right: Dimensions.width20(context), top: Dimensions.height20(context)),
+                      decoration: BoxDecoration(borderRadius: BorderRadius.only(topRight: Radius.circular(Dimensions.radius20(context)), topLeft: Radius.circular(Dimensions.radius20(context))), color: Colors.white),
                       child: Column(
                         crossAxisAlignment: CrossAxisAlignment.start,
                         children: [
-                          AppColumn(
-                              text: data.data?.nama ?? "-",
-                              est: data.data?.estimasi ?? 0),
+                          AppColumn(text: data.data?.nama ?? "-", est: data.data?.estimasi ?? 0),
                           SizedBox(
                             height: Dimensions.height10(context),
                           ),
                           MainText(
                             "Rp ${data.data?.harga.toCurrency()}",
-                            style: TextStyle(
-                                color: AppColors.iconColor2,
-                                fontSize: context.fontSize(25)),
+                            style: TextStyle(color: AppColors.iconColor2, fontSize: context.fontSize(25)),
                           ),
                           const Divider(
                             thickness: 5,
@@ -228,11 +192,7 @@ class _DetailProdukPageState extends State<DetailProdukPage> {
                               Expanded(
                                 child: Align(
                                   alignment: Alignment.centerRight,
-                                  child: IconAndTextWidget(
-                                      icon: Icons.access_time_rounded,
-                                      text:
-                                          "${data.data?.estimasi.toCurrency()} min",
-                                      iconColor: AppColors.iconColor2),
+                                  child: IconAndTextWidget(icon: Icons.access_time_rounded, text: "${data.data?.estimasi.toCurrency()} min", iconColor: AppColors.iconColor2),
                                 ),
                               )
                             ],
@@ -240,10 +200,7 @@ class _DetailProdukPageState extends State<DetailProdukPage> {
                           SizedBox(
                             height: Dimensions.height20(context),
                           ),
-                          Expanded(
-                              child: SingleChildScrollView(
-                                  child: ExpandableTextWidget(
-                                      text: data.data?.deskripsi ?? "-"))),
+                          Expanded(child: SingleChildScrollView(child: ExpandableTextWidget(text: data.data?.deskripsi ?? "-"))),
                           MainTextField(
                             label: "Catatan",
                             controller: catatanCon,
@@ -257,18 +214,8 @@ class _DetailProdukPageState extends State<DetailProdukPage> {
             ),
             bottomNavigationBar: Container(
               height: Dimensions.bottomHeightBar(context),
-              padding: EdgeInsets.only(
-                  top: Dimensions.height30(context),
-                  bottom: Dimensions.height30(context),
-                  left: Dimensions.width20(context),
-                  right: Dimensions.width20(context)),
-              decoration: BoxDecoration(
-                  color: AppColors.buttonBackgroundCOlor,
-                  borderRadius: BorderRadius.only(
-                      topLeft:
-                          Radius.circular(Dimensions.radius20(context) * 2),
-                      topRight:
-                          Radius.circular(Dimensions.radius20(context) * 2))),
+              padding: EdgeInsets.only(top: Dimensions.height30(context), bottom: Dimensions.height30(context), left: Dimensions.width20(context), right: Dimensions.width20(context)),
+              decoration: BoxDecoration(color: AppColors.buttonBackgroundCOlor, borderRadius: BorderRadius.only(topLeft: Radius.circular(Dimensions.radius20(context) * 2), topRight: Radius.circular(Dimensions.radius20(context) * 2))),
               child: Row(
                 children: [
                   Expanded(
@@ -283,17 +230,11 @@ class _DetailProdukPageState extends State<DetailProdukPage> {
                         left: Dimensions.width20(context),
                         right: Dimensions.width20(context),
                       ),
-                      decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(
-                              Dimensions.radius20(context)),
-                          color: Colors.white),
+                      decoration: BoxDecoration(borderRadius: BorderRadius.circular(Dimensions.radius20(context)), color: Colors.white),
                       child: Row(
                         mainAxisAlignment: MainAxisAlignment.spaceAround,
                         children: [
-                          InkWell(
-                              onTap: () => decrease(),
-                              child: const Icon(Icons.remove,
-                                  color: AppColors.signColor)),
+                          InkWell(onTap: () => decrease(), child: const Icon(Icons.remove, color: AppColors.signColor)),
                           SizedBox(
                             width: Dimensions.width10(context) / 2,
                           ),
@@ -301,45 +242,40 @@ class _DetailProdukPageState extends State<DetailProdukPage> {
                           SizedBox(
                             width: Dimensions.width10(context) / 2,
                           ),
-                          InkWell(
-                              onTap: () => increase(),
-                              child: const Icon(Icons.add,
-                                  color: AppColors.signColor))
+                          InkWell(onTap: () => increase(), child: const Icon(Icons.add, color: AppColors.signColor))
                         ],
                       ),
                     ),
                   ),
                   Expanded(
-                    child: InkWell(
-                      onTap: () {
-                        if (widget.kodeKeranjang != null) {
-                          updateKeranjangData();
-                          return;
-                        }
-                        addKeranjang();
-                      },
-                      child: Container(
-                        alignment: Alignment.center,
-                        padding: EdgeInsets.only(
-                            top: Dimensions.height20(context),
-                            bottom: Dimensions.height20(context),
-                            left: Dimensions.width15(context),
-                            right: Dimensions.width15(context)),
-                        decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(
-                                Dimensions.radius20(context)),
-                            color: AppColors.mainColor),
-                        child: BigText(
-                          text: dataBuilder(() {
-                            if (widget.kodeKeranjang != null) {
-                              return "Update Keranjang";
-                            }
-                            return "Tambah Keranjang";
-                          })!,
-                          color: Colors.white,
+                    child: Builder(builder: (context) {
+                      if (data.data?.isActive == false) {
+                        return const SizedBox();
+                      }
+                      return InkWell(
+                        onTap: () {
+                          if (widget.kodeKeranjang != null) {
+                            updateKeranjangData();
+                            return;
+                          }
+                          addKeranjang();
+                        },
+                        child: Container(
+                          alignment: Alignment.center,
+                          padding: EdgeInsets.only(top: Dimensions.height20(context), bottom: Dimensions.height20(context), left: Dimensions.width15(context), right: Dimensions.width15(context)),
+                          decoration: BoxDecoration(borderRadius: BorderRadius.circular(Dimensions.radius20(context)), color: AppColors.mainColor),
+                          child: BigText(
+                            text: dataBuilder(() {
+                              if (widget.kodeKeranjang != null) {
+                                return "Update Keranjang";
+                              }
+                              return "Tambah Keranjang";
+                            })!,
+                            color: Colors.white,
+                          ),
                         ),
-                      ),
-                    ),
+                      );
+                    }),
                   )
                 ],
               ),
